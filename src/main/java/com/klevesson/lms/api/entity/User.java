eae13bc90dc9251fa92223e31e7c0136c9948a01
klevesson <klevesson.developer@gmail.com>
2026-07-01 17:10:04 -0300

    Entidade de usuários

---
+++ src/main/java/com/klevesson/lms/api/entity/User.java
@@ -0,0 +1,90 @@
+package com.klevesson.lms.api.entity;
+
+import java.time.LocalDateTime;
+
+public class User {
+    
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+    private String username;
+
+    private String passwordHash;
+    
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    public User(Long id, String name, String email, String username, String passwordHash, LocalDateTime createdAt, LocalDateTime updatedAt) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.username = username;
+        this.passwordHash = passwordHash;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+
+    public User() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
